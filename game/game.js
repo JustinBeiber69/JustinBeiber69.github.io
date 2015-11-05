2bf6925a70eca0693ebff55b26e0ada40569cb12
shahalyemeni <bsd-12@BSD-12s-iMac.local>
2015-11-05 12:00:12 +0800

    bruh
    bruh

---
+++ game/game.js
@@ -0,0 +1,7 @@
+//js goes here
+$(document).ready(function(){
+    $("#myButton").click(function(){
+        $("#myButton").hide();
+        $("#myBody").css("background-image", "url('Untitled-2.jpg')");
+    });
+ });
