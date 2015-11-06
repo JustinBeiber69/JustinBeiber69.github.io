25b1b52b2c3201bdcee80040cbb6e08c9ed30ae9
shahalyemeni <bsd-12@BSD-12s-iMac.local>
2015-11-06 14:41:24 +0800

    bruh
    bruh

--- game/game.js
+++ game/game.js
@@ -1,7 +1,6 @@
 //js goes here
 $(document).ready(function() {
     $("#myButton").click(function() {
-        $("#myButton").hide();
         window.location="justinbeiber69.github.io/illuminati/" + this.id;
 
 
